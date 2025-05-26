9371fdd5ffbea230a4f945f9a639ae1d40afa15c
ochrebasis <l.dale@bellevuecollege.edu>
2025-05-26 15:03:21 -0700

    fix merging issues number 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
       colors: {
         skin: {
           bg: 'var(--color-bg)',
@@ -17,23 +16,10 @@ module.exports = {
           highlight: 'var(--color-highlight)',
           'accent-bg': 'var(--color-accent-bg)',
         },
-=======
-      skin: {
-          bg: 'var(--color-bg)',
-          text: 'var(--color-text)',
-          accent: 'var(--color-accent)',
-          muted: 'var(--color-muted)',
->>>>>>> d9f6b75eb883f2dd954ce3bd7d7e5127e5ba7999
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
-<<<<<<< HEAD
-=======
-      backgroundImage: {
-        'spotlight': 'radial-gradient(...)' // optional if you want reusable class
-      },
->>>>>>> d9f6b75eb883f2dd954ce3bd7d7e5127e5ba7999
     },
   },
   plugins: [],
