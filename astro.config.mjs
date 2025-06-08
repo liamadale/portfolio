ee0e4f8e9fd568b16a47ba5519cc6cf01d1431d7
ochrebasis <l.dale@bellevuecollege.edu>
2025-06-08 10:34:42 -0700

    broken image shortcode support :( needs fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import remarkShortcodes from './src/plugins/remark-shortcodes.js';
+import { rehypePrism } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
+    mdx(),
   ],
+  markdown: {
+    remarkPlugins: [remarkShortcodes],
+    rehypePlugins: [rehypePrism],
+  },
+  content: {
+    experimental: {
+      strict: true,
+    },
+  },
 });
--- src/content/blog/another-blog-post-2.md
+++ src/content/blog/another-blog-post-2.md
@@ -1,8 +1,22 @@
 ---
 title: Hello World
-pubDate: 2025-1-26
+pubDate: "2025-01-26"
+description: "First blog post."
 category: general
 tags:
   - intro
 ---
 
+## BEHOLD AN IMAGE:
+
+{{< ImageShortcode src="/assets/images/logo.jpg" alt="Astro logo" width="256" height="256" >}}
+
+<div class="bg-red-500 text-white p-2">
+  <p>Shortcode loaded ✅</p>
+  <p>src: {Astro.props.src}</p>
+</div>
+
+
+<p style="color: lime;">✅ Markdown continues below</p>
+
+## ↑↑ ISN'T IT BEAUTIFUL? ↑↑
