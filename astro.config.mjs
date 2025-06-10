bd1b361348ca1aedc930763dcd7155d1ddce2386
ochrebasis <l.dale@bellevuecollege.edu>
2025-06-10 00:36:13 -0700

    Got rid of not working implementation, will pursue working shortcodes in the future.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import remarkImageShortcode from './src/plugins/remarkImageShortcode.js';
-import debugPlugin from './src/plugins/debugPlugin.js';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
   ],
-  markdown: {
-    remarkPlugins: [debugPlugin, remarkImageShortcode],
-  },
 });
